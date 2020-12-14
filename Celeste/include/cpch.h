ed1b40fa8060f5ff3524acd78f0f41b46860e874
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2020-12-14 12:36:52 -0500

    Add main files

---
+++ Celeste/include/cpch.h
@@ -0,0 +1,8 @@
+//
+// Created by nate on 12/14/20.
+//
+
+#ifndef CELESTE_CPCH_H
+#define CELESTE_CPCH_H
+
+#endif //CELESTE_CPCH_H
---
+++ Celeste/src/Celeste.cpp
@@ -0,0 +1,7 @@
+#include <Celeste.h>
+
+#include <iostream>
+
+void hello() {
+    std::cout << "Hello, World!" << std::endl;
+}
